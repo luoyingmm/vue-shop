138409be235d5d4f701a65e537d1fbb2ff69751b
Suemor <1502972236@qq.com>
2021-12-04 20:07:16 +0800

    首页

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,19 +1,34 @@
 <template>
   <div>
     <TypeNav />
-    <ListContainer/>
+    <ListContainer />
+    <Recommend />
+    <Rank />
+    <Like />
+    <Floor />
+    <Floor />
+    <Brand />
   </div>
 </template>
 
 <script>
-import ListContainer from '@/pages/Home/ListContainer';
+import ListContainer from "@/pages/Home/ListContainer";
+import Recommend from "@/pages/Home/Recommend";
+import Rank from "@/pages/Home/Rank";
+import Like from "@/pages/Home/Like";
+import Floor from "@/pages/Home/Floor";
+import Brand from "@/pages/Home/Brand";
 export default {
-  components:{
-    ListContainer
-  }
+  components: {
+    ListContainer,
+    Recommend,
+    Rank,
+    Like,
+    Floor,
+    Brand,
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-
 </style>
