8bf63025aaec3614db2dfb678df0a908d6359a63
Suemor <1502972236@qq.com>
2021-12-04 14:45:51 +0800

    router

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <Header></Header>
+    <router-view></router-view>
     <Footer></Footer>
   </div>
 </template>
---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+import Home from '@/pages/Home'
+import Search from '@/pages/Search'
+import Login from '@/pages/Login'
+import register from '@/pages/Register'
+export default new VueRouter({
+    routes: [
+        {
+            path: '/home',
+            component: Home
+        },
+        {
+            path: '/search',
+            component: Search
+        },
+        {
+            path: '/login',
+            component: Login
+        },
+        {
+            path: '/register',
+            component: register
+        }    
+
+        ]
+    });
