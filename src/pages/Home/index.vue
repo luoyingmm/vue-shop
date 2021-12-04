5e3ff012358b3a42cf264fac8500a73df82fafc4
Suemor <1502972236@qq.com>
2021-12-04 19:41:10 +0800

    轮播图

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
     <TypeNav />
+    <ListContainer/>
   </div>
 </template>
 
 <script>
+import ListContainer from '@/pages/Home/ListContainer';
 export default {
-    
+  components:{
+    ListContainer
+  }
 };
 </script>
 
 <style lang="scss" scoped>
+
 </style>
